afd97199fbcaf9409a4d0feb66e218fdf28a2587
Dark3aterMidir669 <ayman.namous2020@gmail.com>
2026-05-07 20:19:18 +0100

    Delete solver directory

--- solver/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Module solver : Contient les classes de résolution du problème d'optimisation.
-"""
-
-from .optimization_problem import ProblemeOptimisation
-from .simplex_solver import SimplexeSolver
-
-__all__ = ['ProblemeOptimisation', 'SimplexeSolver']
