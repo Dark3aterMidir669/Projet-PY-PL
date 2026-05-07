d56e99cbe60dbb2aef44cdf9132be7250f405f1a
Dark3aterMidir669 <ayman.namous2020@gmail.com>
2026-05-07 20:19:45 +0100

    Delete utils directory

--- utils/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-Module utils : Fonctions utilitaires.
-"""
-
-from .helpers import afficher_banner, afficher_separation, arrondir_solution, afficher_info_systeme
-
-__all__ = ['afficher_banner', 'afficher_separation', 'arrondir_solution', 'afficher_info_systeme']
