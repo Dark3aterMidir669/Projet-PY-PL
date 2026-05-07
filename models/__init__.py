4c61a4ce4623b75ff032680da53c5ba2b85bd3c1
Dark3aterMidir669 <ayman.namous2020@gmail.com>
2026-05-07 20:19:08 +0100

    Delete models directory

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Module models : Contient les classes de modélisation des produits et ressources.
-"""
-
-from .product import Produit, Lait, Yaourt, Fromage
-from .resource import Ressource
-
-__all__ = ['Produit', 'Lait', 'Yaourt', 'Fromage', 'Ressource']
